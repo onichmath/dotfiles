a23b49ea49061eaa2a599ceeba05d4ae1e4c49df
Matthew <mathonich@yahoo.com>
2023-05-17 00:21:08 -0700

    Moduler part 2

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -16,7 +16,7 @@ require('telescope').setup {
 pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>O', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
